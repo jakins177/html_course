16f763d88eeae8774b81798a3e5c7760d7f8b51b
jakins177 <58919376+jakins177@users.noreply.github.com>
2025-09-01 15:01:06 -0500

    Update gasergy display in AI chat

--- gasergy/decrease_gasergy.php
+++ gasergy/decrease_gasergy.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/decrease_gasergy.log');
+$logFile = __DIR__ . '/decrease_gasergy.log';
 
 session_start();
 
